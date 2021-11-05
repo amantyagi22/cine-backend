71a00957f0e99088a5d5fa5dd46bbb7001de0e2a
amantyagi22 <amantyagi2k@gmail.com>
2021-11-06 00:20:18 +0530

    Updated File

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+
 const userRoute = require("./routes/user");
 const questionRoute = require("./routes/question");
-
 const allQuestions = require("./routes/getAllQuestions");
 const allCandidates = require("./routes/getAllCandidates");
-const SubmitQuestion = require('./routes/submitQuestion')
-const LoginAndRegisterRoute = require('./routes/login')
-const cors = require('cors')
+const SubmitQuestion = require("./routes/submitQuestion");
+const LoginAndRegisterRoute = require("./routes/login");
+const cors = require("cors");
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL, {})
+mongoose
+  .connect(process.env.MONGO_URL, {})
   .then(() => {
     console.log(`Connected to MONGODB`);
   })
@@ -20,9 +21,9 @@ mongoose.connect(process.env.MONGO_URL, {})
   });
 
 //Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use("/api/sign",LoginAndRegisterRoute)
+app.use("/api/sign", LoginAndRegisterRoute);
 app.use("/api/users", userRoute);
 app.use("/api/questions", questionRoute, allQuestions);
 app.use("/api/candidates", allCandidates);
